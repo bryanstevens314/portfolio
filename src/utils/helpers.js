45155d81cd369aed04a5c9aff26c5106efb91cae
Bryan Stevens <b.stevens.photo@gmail.com>
2023-06-06 09:05:05 -0700

    Updated about copy

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -13,11 +13,11 @@ export const BUY_ME_A_COFFEE = 'https://www.buymeacoffee.com/bstevens';
 export const PAYPAL = 'https://www.paypal.com/paypalme/my/profile';
 export const GITHUB_SPONSORS = 'https://github.com/sponsors/bryanstevens314?frequency=recurring&sponsor=bryanstevens314';
 
-export const ABOUT = `I'm a software engineer with a passion for creating innovative and intuitive technology solutions. With over 9 years of experience in the industry I have honed my skills in a wide range of programming languages and software development methodologies.
+export const ABOUT = `I'm a software engineer with a passion for creating innovative and intuitive technology solutions. With nearly a decade of experience in the industry I have honed my skills in a wide range of programming languages and software development methodologies.
 
 I have a strong understanding of both the technical and creative aspects of software engineering allowing me to bring a unique perspective to every project I work on. Whether it's developing a mobile app, creating a web-based platform, or anything in between I'm always striving to push the boundaries of what's possible.
 
-When I'm not coding I enjoy staying up-to-date with the latest trends and advancements in the tech industry as well as experimenting with new technologies. I'm also an avid gamer and love to spend my free time immersed in virtual worlds.`
+When I'm not coding I enjoy staying up-to-date with the latest trends and advancements in the tech industry as well as experimenting with new technologies.`
 
 export const DONATE_TEXT = `Thank you for considering a donation to support my work. Your contribution will help me to continue developing innovative and impactful technology solutions.
 
